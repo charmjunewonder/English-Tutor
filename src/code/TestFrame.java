e0a9de421ca660ba621aaaec6872afe7d92a3bde
Lyshen <815604679@qq.com>
2012-05-30 14:20:35 +0800

    whatever u done

--- src/code/TestFrame.java
+++ src/code/TestFrame.java
@@ -4,13 +4,15 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.ImageIcon;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.Color;
 
 public class TestFrame extends AbstractFrame{
-    private JLabel titleLabel,questionLabel,answerLabel;
+    private JLabel titleLabel,questionLabel,answerLabel,processLabel[],processFeather;
     private JTextField answerTextField,questionTextField;
 	private JButton nextButton,soundButton;
+	private int now,sum;
 	
 	public TestFrame(){
 		super("resource/TestFrame.png");
@@ -20,6 +22,11 @@ public class TestFrame extends AbstractFrame{
 		initQuestionTextField();
 		initAnswerLabel();
 		initAnswerTextField();
+		initNextButton();
+		initProcessLabel();
+		initProcessFeather();
+	    now=0;
+	    sum=9;
 	}
 	
 	private void initTitleLabel(){
@@ -39,28 +46,69 @@ public class TestFrame extends AbstractFrame{
 	
 	private void initQuestionLabel(){
 		questionLabel = new JLabel("Question:");
-		questionLabel.setBounds(80, 150, 300, 50);
+		questionLabel.setBounds(80, 180, 300, 50);
 		getContentPane().add(questionLabel);
 	}
 	
 	private void initQuestionTextField(){
 		questionTextField = new JTextField();
 		questionTextField.setEditable(false);
-		questionTextField.setBounds(130, 150, 300, 40);
+		questionTextField.setBounds(130, 180, 300, 40);
 		getContentPane().add(questionTextField);
 	}
 	
 	private void initAnswerLabel(){
 		answerLabel = new JLabel("Answer:");
-		answerLabel.setBounds(80,200,300,50);
+		answerLabel.setBounds(80,235,300,50);
 		getContentPane().add(answerLabel);
 	}
 	
 	private void initAnswerTextField(){
 		answerTextField = new JTextField();
-		answerTextField.setBounds(130, 205, 300, 40);
+		answerTextField.setBounds(130, 240, 300, 40);
 		answerTextField.setEditable(true);
 		getContentPane().add(answerTextField);
+	}
+	
+	private void initNextButton(){
+		nextButton = new JButton(new ImageIcon("resource/next.png"));
+		nextButton.setBackground(new Color(0,0,0,0));
+		nextButton.setOpaque(false);
+		nextButton.setBorderPainted(false);
+		nextButton.setBounds(400, 400, 100, 100);
+		nextButton.addMouseListener(new NextButtonMouseAdapter());
+		getContentPane().add(nextButton);
+	}
+	
+	private void initProcessLabel(){
+		processLabel = new JLabel[11];
+		for(int i=0;i<10;i++){
+			processLabel[i] = new JLabel((i+1)+"");
+		    processLabel[i].setBounds(50+i*30,500,15,15);
+		    processLabel[i].setVisible(false);
+		    getContentPane().add(processLabel[i]);
+		}
+		processLabel[0].setVisible(true);
+	}
+	
+	private void initProcessFeather(){
+		processFeather = new JLabel(new ImageIcon("resource/processFeather.png"));
+	    processFeather.setBounds(40, 410, 100, 90);
+	    getContentPane().add(processFeather);
+	}
+	
+	private void nextQuestionMotion(){
+		if (now<sum) {
+		  processFeather.setBounds(45+(now+1)*30,410,100,90);
+		  processLabel[now+1].setVisible(true);
+		  now++;
+		}
+	}
+	
+	private class NextButtonMouseAdapter extends MouseAdapter{
+		public void mouseClicked(MouseEvent e){
+			nextQuestionMotion();
+		}
 	}
 	
 	public static void main(String args[]){
